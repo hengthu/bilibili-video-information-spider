4fa44a4a5b3c29016bda73b524bc67923ca2b9f9
zhang0peter <zsd780885029@gmail.com>
2018-03-03 18:20:13 +0800

    bilibili-spider

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# bilibili-spider
-B站视频信息爬虫
+## B站视频信息爬虫
+
+在参考 [bili-spider](https://github.com/chenjiandongx/bili-spider)的代码后，
+我在他的代码的基础上进行改进，并最终做出成果(已经把我的代码pull requests了)。
+
+
+
+```python
+pip install requests,concurrent,sqlite3,threading,time
+```
+
---
+++ bilibili-spider.py
@@ -0,0 +1,93 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  3 17:48:23 2018
+
+@author: peter
+"""
+import threading
+import time
+import sqlite3
+import requests
+from concurrent import futures
+
+headers = {
+    'X-Requested-With': 'XMLHttpRequest',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36'
+                  '(KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+}
+total = 1
+result = []
+lock = threading.Lock()
+flag = None
+conn = None
+
+def run(url):
+    # 启动爬虫
+    global total
+    req = requests.get(url, headers=headers, timeout=6).json()
+    time.sleep(0.4)     # 延迟，避免太快 ip 被封
+    try:
+        data = req['data']
+        video = (
+            total,
+            data['aid'],        # 视频编号
+            data['view'],       # 播放量
+            data['danmaku'],    # 弹幕数
+            data['reply'],      # 评论数
+            data['favorite'],   # 收藏数
+            data['coin'],       # 硬币数
+            data['share']       # 分享数
+        )
+        with lock:
+            result.append(video)
+            if total %250 == 0:
+                print(total)
+            total += 1
+    except:
+        pass
+
+def create():
+    #创建数据库
+    global conn
+    conn = sqlite3.connect('data.db')
+    conn.execute("""create table if not exists data
+                    (id int prinmary key autocrement,
+                    aid int,
+                    view int,
+                    danmaku int,
+                    reply int,
+                    favorite int,
+                    coin int,
+                    share int)""")
+    conn.execute("""insert into data
+                    (id,aid,view,danmaku,reply,favorite,coin,share)    
+                    values(0,0,0,0,0,0,0,0)""")
+
+def save():
+    # 将数据保存至本地
+    global result,conn,flag,total
+    command = "insert into data \
+             values(?,?,?,?,?,?,?,?);"
+    for row in result:
+        try:
+            conn.execute(command,row)
+        except:
+            conn.rollback()
+            flag = "error has occurred when total is "+str(total)
+            pass
+    conn.commit()
+    result = []
+
+
+if __name__ == "__main__":
+    create()
+    for i in range(0,1981): 
+        begin = 10000*i
+        urls = ["http://api.bilibili.com/archive_stat/stat?aid={}".format(j)
+        for j in range(begin,begin+10000)]
+        with futures.ThreadPoolExecutor(32) as executor:
+                executor.map(run, urls)
+        save()
+    if flag != None:
+        print(flag)
+    conn.close()
